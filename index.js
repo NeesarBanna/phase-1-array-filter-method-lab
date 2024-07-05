f557040bc1a841ba1773c111b295e0fdb7169fa5
Neesar Banna <Bannasqe@gmail.com>
2024-07-05 16:55:57 -0400

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    let matchingDrivers = [];
+  
+    for (let driver of drivers) {
+      if (driver.toLowerCase() === string.toLowerCase()) {
+        matchingDrivers.push(driver);
+      }
+    }
+  
+    return matchingDrivers;
+  }
+  
+  let drivers = ['John', 'Mary', 'Mike', 'Sarah', 'Peter', 'Joe'];
+  let searchString = 'Mike';
+  
+  console.log(findMatching(drivers, searchString));
+
+  function fuzzyMatch(drivers, findMatchingLetter) {
+    const filteredDrivers = drivers.filter(driver => {
+    return driver.startsWith(findMatchingLetter);
+    });
+    
+    return filteredDrivers;
+    }
+
+    function matchName(drivers, query) {
+        const filteredDrivers = drivers.filter(driver => {
+          return driver.name === query;
+        });
+      
+        return filteredDrivers;
+      }
